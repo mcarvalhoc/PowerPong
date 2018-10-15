e29b15e0c46698d6360e9625ef6d59d57b9a5027
Matheus Carvalho Couto <matheus.couto@agrario.gov.br>
2018-10-15 13:24:58 -0300

    Correcao de erros

--- PlacarTime.java
+++ PlacarTime.java
@@ -1,38 +1,58 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class PlacarTime here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Write a description of class TimeMinuto here. @author (your name) @version (a version number or a date)
  */
 public class PlacarTime extends Actor
 {
-    public int valor = 20;
+    /* WARNING: This file is auto-generated and any changes to it will be overwritten*/
+    /* (World, Actor, GreenfootImage, Greenfoot and MouseInfo)*/
+    public int valor = 300;
 
     /**
-     * Act - do whatever the PlacarTime wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * 
      */
-    public void act() 
+    public PlacarTime()
     {
         atualizaImagem(valor);
-       
-        
-        
+    }
 
-    }    
+    /**
+     * Act - do whatever the PlacarTime wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        atualizaImagem(valor);
+        atualizaImagem();
+    }
 
-    private void atualizaImagem(int valor){
-        GreenfootImage Time = new GreenfootImage(converteNumero(valor), 24, Color.WHITE, new Color(0,0,0,0),Color.BLACK);
+    /**
+     * 
+     */
+    private void atualizaImagem(int valor)
+    {
+        GreenfootImage Time =  new  GreenfootImage(converteNumero(valor), 24, Color.WHITE,  new  Color(0, 0, 0, 0), Color.BLACK);
         setImage(Time);
     }
 
-    public PlacarTime(){
-        atualizaImagem(valor);
+    /**
+     * 
+     */
+    public void atualizaImagem()
+    {
+        float resultado = 300;
+        resultado = getWorldOfType(MyWorld.class).getCiclo() % 8;
+        if ((resultado > 6)&&(valor !=0)){
+            valor = valor - 1;
+        }
+       
     }
 
-    private String converteNumero(int valor){
-        return String.format("%02d", valor);
-    }  
+    /**
+     * 
+     */
+    private String converteNumero(int valor)
+    {
+        return String.format("%03d", valor);
+    }
 }
